dc1e96acf60be23ed3b80ed79212b98e2e279ba5
Adam Verma <vermaadam@gmail.com>
2020-08-05 17:42:01 -0500

    server.js - adding catch-all to app for static build, and modified mongo.connect

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ require("dotenv").config({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
+  app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 }
 // Add routes, both API and view
 app.use(routes);
@@ -35,7 +38,7 @@ app.use(bodyParser.json());
 // Connect to Mongo DB
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/portfolio";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, err => { if(err) { console.log(err); }}).
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, err => { if(err) { console.log(err); }}).
 then(() => console.log("MONGO DATABASE CONNECTED"));
 
 // Start the API server
