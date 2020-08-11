983bef69728d84a5b94b9dfbdd50195c546d1dc7
Adam Verma <vermaadam@gmail.com>
2020-08-10 23:25:17 -0500

    server.js = changing static file on production build:

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ require("dotenv").config({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.static("client/build"));
-  app.get('*', (req, res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
+  app.use(express.static("client/build"));
+  // app.get('*', (req, res) =>{
+  //   res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  // });
 }
 // Add routes, both API and view
 app.use(routes);
